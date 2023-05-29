a30e219488b522079ecda7dc6f93958a25c60997
Arthur Lafrance <lafrancearthur@gmail.com>
2023-05-29 01:30:07 -0700

    dead simple initial prototype

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,72 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use std::marker::PhantomData;
+
+// TODO: would probably be nice to be able to set an upper bound on the size of the buffer
+
+// TODO: would probably be better to manage the allocation directly rather than delegating to Vec.
+// I think the finer control would be nice and would possibly make the interface nicer (like the sizes
+// being in terms of elements rather than bytes is kind of wonky)
+
+/* --- Reservoir --- */
+
+#[derive(Clone)]
+pub struct Reservoir<T>(Vec<T>);
+
+impl<T> Reservoir<T> {
+    pub const SMALL_CAPACITY: usize = 64;
+    pub const MEDIUM_CAPACITY: usize = 256;
+    pub const LARGE_CAPACITY: usize = 1024;
+
+    pub fn new(capacity: usize) -> Self {
+        Reservoir(Vec::with_capacity(capacity))
+    }
+
+    pub fn get(&self, handle: Handle<T>) -> &T {
+        &self.0[handle.0]
+    }
+
+    pub fn get_mut(&mut self, handle: Handle<T>) -> &mut T {
+        &mut self.0[handle.0]
+    }
+
+    pub fn insert(&mut self, data: T) -> Handle<T> {
+        self.0.push(data);
+        Handle(self.0.len() - 1, PhantomData)
+    }
 }
+
+impl<T> Default for Reservoir<T> {
+    fn default() -> Self {
+        Reservoir::new(Self::MEDIUM_CAPACITY)
+    }
+}
+
+/* --- Handle --- */
+
+pub struct Handle<T>(usize, PhantomData<T>);
+
+impl<T> Clone for Handle<T> {
+    fn clone(&self) -> Self {
+        Handle(self.0, self.1)
+    }
+}
+
+impl<T> Copy for Handle<T> {}
+
+// TODO: have to manually impl Debug, Eq, Hash, Ord, PartialEq, PartialOrd
+
+/* --- Tests --- */
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn sanity_check() {
+        let mut r: Reservoir<String> = Reservoir::default();
+        let h = r.insert(String::from("hello"));
+        assert_eq!("hello", r.get(h));
+
+        r.get_mut(h).push('!');
+        assert_eq!("hello!", r.get(h));
     }
 }
